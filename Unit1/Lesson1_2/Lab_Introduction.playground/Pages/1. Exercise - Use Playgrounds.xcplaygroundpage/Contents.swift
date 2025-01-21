865c63beb6785466bfc661c53e1c6ff4a14459d2
hillc5621 <hillc5621@durhamtech.edu>
2025-01-21 15:06:08 -0500

    Completed Lab 1-2

--- Unit1/Lesson1_2/Lab_Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
+++ Unit1/Lesson1_2/Lab_Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
@@ -10,14 +10,15 @@ print("How to use playgrounds to make writing Swift fun and simple")
 /*:
  Now print your own phrases to the console. Pick one of your favorite songs. Use your knowledge of the `print` function to display the song title and artist.
  */
-
+print("Song: M.I.A")
+print("Artist: Avenged Sevenfold")
 
 /*:
  Use multiple `print` functions to write out some of the lyrics to the song.
  */
-
-
-
+print("The fighting rages on")
+print("to challenge me you must be strong")
+print("I walk your land but dont belong")
 /*:
 
  _Copyright © 2023 Apple Inc._
