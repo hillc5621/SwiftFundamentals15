3d12988a4f6e1d235669d72b76d4c8fe6a8b92c5
hillc5621 <hillc5621@durhamtech.edu>
2025-02-10 19:15:07 -0500

    Completed Lesson 2_3

--- Unit2/Lesson2_3/Lab_Functions.playground/Pages/4. App Exercise - Progress Updates.xcplaygroundpage/Contents.swift
+++ Unit2/Lesson2_3/Lab_Functions.playground/Pages/4. App Exercise - Progress Updates.xcplaygroundpage/Contents.swift
@@ -9,10 +9,37 @@
  
  Call the function a number of times, passing in different values of `steps` and `goal`. Observe the printouts and make sure what is printed to the console is what you would expect for the parameters passsed in.
  */
-
+func progressUpdate (steps : Int , goal : Int){
+    if (steps < goal * 1/10) {
+        print("You're of to a good start.")
+    }
+    else if (steps < goal * 1/2) {
+        print("You're almost halfway there!")
+    }
+    else if (steps < goal * 9/10) {
+        print("You're almost there!")
+    }
+    else if (steps < goal) {
+        print("You're so close!")
+    }
+    else {
+        print("You beat your goal!")
+    }
+}
+progressUpdate(steps: 250, goal: 1000)
+progressUpdate(steps: 5000, goal: 10000)
+progressUpdate(steps: 90000, goal: 100000)
 
 //:  Your fitness tracking app is going to help runners stay on pace to reach their goals. Write a function called pacing that takes four `Double` parameters called `currentDistance`, `totalDistance`, `currentTime`, and `goalTime`. Your function should calculate whether or not the user is on pace to hit or beat `goalTime`. If yes, print "Keep it up!", otherwise print "You've got to push it just a bit harder!"
-
+func pacing (currentDistance : Double, totalDistance : Double, currentTime: Double, goalTime: Double) {
+    if ((currentDistance + currentTime) < ((totalDistance + goalTime) * 1/2)) {
+        print("You've got to push it just a bit harder")
+    }
+    else{
+        print("Keep it up!")
+    }
+}
+pacing(currentDistance: 675, totalDistance: 1000, currentTime: 35, goalTime: 60)
 
 /*:
 [Previous](@previous)  |  page 4 of 6  |  [Next: Exercise - Return Values](@next)
--- Unit2/Lesson2_3/Lab_Functions.playground/Pages/5. Exercise - Return Values.xcplaygroundpage/Contents.swift
+++ Unit2/Lesson2_3/Lab_Functions.playground/Pages/5. Exercise - Return Values.xcplaygroundpage/Contents.swift
@@ -3,11 +3,19 @@
 
  Write a function called `greeting` that takes a `String` argument called name, and returns a `String` that greets the name that was passed into the function. I.e. if you pass in "Sophie" the return value might be "Hi, Sophie! How are you?" Use the function and print the result.
  */
-
-
+func greeting (name : String) -> String {
+    
+    return ("Hi ," + name + " How are you?")
+}
+    
+print(greeting(name: "Caleb"))
 //:  Write a function that takes two `Int` arguments, and returns an `Int`. The function should multiply the two arguments, add 2, then return the result. Use the function and print the result.
-
-
+func Multiply (Num1 : Int, Num2 : Int) -> Int {
+    
+   var Num3  = ((Num1 * Num2) + 2)
+    return Num3
+}
+print(Multiply(Num1: 10, Num2: 3))
 /*:
 [Previous](@previous)  |  page 5 of 6  |  [Next: App Exercise - Separating Functions](@next)
  */
