129787f75385e13eb1a4ae9eab82d3a514a3e1d9
hillc5621 <hillc5621@durhamtech.edu>
2025-02-25 11:07:35 -0500

    Lesson 2_9 Completed

---
+++ Unit2/Lesson2_9/Hobby Tutorial/Hobby TutorialTests/Hobby_TutorialTests.swift
@@ -0,0 +1,17 @@
+//
+//  Hobby_TutorialTests.swift
+//  Hobby TutorialTests
+//
+//  Created by spring2025 on 2/25/25.
+//
+
+import Testing
+@testable import Hobby_Tutorial
+
+struct Hobby_TutorialTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
